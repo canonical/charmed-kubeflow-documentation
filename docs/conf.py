98f2386cfa045f54508cfb66aae94d8765dbd7b6
Michael Park <michael.park@canonical.com>
2025-08-01 09:28:49 +1000

    fix: remove versioning from sitemap

--- docs/conf.py
+++ docs/conf.py
@@ -182,11 +182,7 @@ html_baseurl = 'https://documentation.ubuntu.com/charmed-kubeflow/'
 # URL scheme. Add language and version scheme elements.
 # When configured with RTD variables, check for RTD environment so manual runs succeed:
 
-if 'READTHEDOCS_VERSION' in os.environ:
-    version = os.environ["READTHEDOCS_VERSION"]
-    sitemap_url_scheme = '{version}{link}'
-else:
-    sitemap_url_scheme = 'MANUAL/{link}'
+sitemap_url_scheme = '{link}'
 
 # Include `lastmod` dates in the sitemap:
 
