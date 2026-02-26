f245de36ec444fea97dd14405335a6ac05cd203d
Dario Faccin <dario.faccin@canonical.com>
2026-02-26 16:10:09 +0100

    chore: exclude Kubeflow links from check
    Signed-off-by: Dario Faccin <dario.faccin@canonical.com>

--- docs/conf.py
+++ docs/conf.py
@@ -248,6 +248,9 @@ linkcheck_ignore = [
     "https://www.terraform.io/",
     "https://github.com/search?q=org%3Acanonical+topic%3Acharm+topic%3Akubeflow&type=repositories",
     "https://github.com/search?q=org%3Acanonical+topic%3Arocks+topic%3Akubeflow&type=repositories",
+    "https://www.kubeflow.org/docs/components/katib/user-guides/trial-template/",
+    "https://www.kubeflow.org/docs/components/central-dash/overview/",
+    "https://v1-9-branch.kubeflow.org/docs/components/katib/user-guides/trial-template/",
 ]
 
 # A regex list of URLs where anchors are ignored by 'make linkcheck'
